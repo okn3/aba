bc05b1f3e1146ea97977370aa876c8057cadf35d
okunoryo <okuno.ryo.411@gmail.com>
2014-09-22 23:31:22 +0900

    グラフ表示のデータを６０に指定

---
+++ data_show_0922.py
@@ -0,0 +1,133 @@
+# -*- coding: utf-8 -*-
+
+import serial
+import time
+import os
+import datetime
+import shlex
+import RPi.GPIO as GPIO
+
+#メール送信
+def sendMail():
+    if cond == 'uri':
+        os.system("sendmail -t < detect_uri.txt")
+    elif cond == 'eva':
+        os.system("sendmail -t < detect_eva.txt")
+
+def main():
+
+    #arduinoの初期化
+##    ser = serial.Serial('/dev/ttyACM0', 9600)
+##    time.sleep(2)
+##    ser.write('0')
+
+    con=serial.Serial('/dev/ttyAMA0', 19200, timeout=10)
+    print con.portstr
+    con.flushInput()
+    sreadTime = datetime.datetime.today()
+    stime = ('lifilm_data/lifilm%s_%s_%s_%s.csv' % (sreadTime.year, sreadTime.month, sreadTime.day, sreadTime.hour))
+   #print stime
+    f = open( stime , 'a')
+    f.close()
+
+    cont = 0
+    x = 0
+    global cond
+
+    while (x < 59): 
+        lop = 33
+        add1 = 0
+        add2 = 0
+        add3 = 0
+        realtemp = 0
+        humidity = 0
+        humidity1 = 0
+        humidity2 = 0
+        realhumidity = 0
+        gus = 0
+        for i in range(lop):
+            str=con.readline()
+            try:
+                add = str
+                items = shlex.split(add)
+#                print items #test
+                ad1 = float(items[2].rstrip(','))
+                ad2 = float(items[3].rstrip(','))
+                ad3 = float(items[4])
+            
+                add1 = add1 + ad1
+                add2 = add2 + ad2
+                add3 = add3 + ad3
+            except:
+                print "detect error"
+
+        realtemp = (1.750-add1/lop)*125
+        humidity = ((add2/lop)/5-0.1515)/0.00636
+        gus = add3/lop*200
+        
+        print ('temp%.1f humidity%.1f gus%.3f' % (realtemp,humidity,gus))
+        print add1/lop,add2/lop,add3/lop
+        
+        # 条件分岐(リアルタイム)
+        if humidity > 45 :
+            cond = "uri"
+##            ser.write('a') #arudno光らす
+            ##os.system("sendmail -t < detect_uri.txt")
+        else:
+            if gus > 90 :
+                cond = "eva"
+##                ser.write('a') #arduno光らす
+                ##os.system("sendmail -t < detect_eva.txt")
+            else:
+##                ser.write('0') #arudno止める
+                cond ="normal"
+        print "condition:" ,cond
+
+     #1分後の処理
+        if cont == 10 :		
+            #csvにデータの書き込み
+            f = open( stime , 'a')
+            readTime = datetime.datetime.today()
+            dy=time.strftime('%y/%m/%d %H:%M',time.localtime())
+            f.write('%s , %.2f, %.2f, %.3f, %s\n' % (dy,realtemp,humidity,gus,cond))
+            f.close()
+            
+            #realtime.csvを更新
+            f = open( 'realtime.csv' , 'a')
+            csv_count = sum(1 for line in open('realtime.csv'))
+            #表示用のcsvファイル作成
+            if csv_count > 60 :
+                os.system("tail -60 realtime.csv > graphview.csv")
+            readTime = datetime.datetime.today()
+            dy=time.strftime('%y/%m/%d %H:%M',time.localtime())
+            f.write('%s , %.2f, %.2f, %.3f, %s\n' % (dy,realtemp,humidity,gus,cond))
+            f.close()
+
+            print "save!"
+            cont = 1
+            x += 1
+
+            # メール送信
+         ##   sendMail()
+
+            #グラフ表示用のrealtime.csvをアップロード
+            scp_cmd_r = "scp -i okn_ubu1.pem graphview.csv ubuntu@54.92.62.157:/var/www/html/aba_chart/HighChart/"
+            os.system(scp_cmd_r)
+            
+        else :
+            cont += 1
+            print "cont:" ,cont
+            print("---------------------------------")
+        print "x:", x
+    
+    #１時間後
+
+    #awsにファイルアップロード
+    scp_cmd = "scp -i okn_ubu1.pem "+stime+" ubuntu@54.92.62.157:~/lifilm_data"
+    os.system(scp_cmd)
+    #ファイル初期化
+    os.system("cat graphview.csv > realtime.csv")
+    print "end"
+
+if __name__ == '__main__':
+    main()
